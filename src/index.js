6d5cfd43c82316f3ea15f7b7d193370a92cbd3e2
NMacOmega <nathaniel.maciver@gmail.com>
2022-06-21 18:32:52 -0400

    PWA Build

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <MapBoxProvider>
-    <App />
-  </MapBoxProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <MapBoxProvider>
+      <App />
+    </MapBoxProvider>
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
